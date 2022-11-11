9377ea5626317caa0a4a18caa64850cfb26f18bd
hackermon <60828015+hackermondev@users.noreply.github.com>
2022-11-11 00:44:23 +0000

    fix ratelimit issue

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const { createInterface } = require('node:readline');
-const { Client, REST, Routes } = require('discord.js');
+const { execSync } = require('child_process');
+const fetch = require('node-fetch');
+const { Client, REST, Routes, GatewayIntentBits } = require('discord.js');
 
-const client = new Client({ intents: [] });
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 const rl = createInterface({ input: process.stdin, output: process.stdout });
 
 
@@ -14,6 +16,16 @@ const question = (q) => new Promise((resolve) => rl.question(q, resolve));
   const token = await question('Application token? ');
   if(!token) throw new Error('Invalid token');
 
+  const ratelimitTest = await fetch(`https://discord.com/api/v9/invites/discord-developers`);
+
+  if(!ratelimitTest.ok) {
+    await question(`Uh oh, looks like the node you're on is currently being blocked by Discord. Press the "Enter" button on your keyboard to be reassigned to a new node. (you'll need to rerun the program once you reconnect)`)
+
+    // This kills the container manager on the repl forcing Replit to assign the repl to another node with another IP address (if the ip is globally rate limited)
+    execSync('kill 1');
+    return;
+  };
+  
   await client.login(token).catch((err) => {
     throw err
   });
