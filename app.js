ac9dd3cede63a8f8ce533d13ea385fd0b35f3c51
hackermon <therealhackermon@gmail.com>
2022-11-16 21:37:22 -0500

    cleanup

--- app.js
+++ app.js
@@ -2,33 +2,33 @@ const { createInterface } = require('node:readline');
 const { execSync } = require('child_process');
 const fetch = require('node-fetch');
 const { Client, Routes } = require('discord.js');
-//Pre-built AND changeable command
-var ping = {
+
+const ping = {
   name: 'ping',
   description: 'yes'
-}
-//Command Example
+};
+
+
+// Command Example
 var command2 = {
   name:'command2',
   description:'yes'
 }
 
-const commands = [ping, command2]; //Add your commands with commas to add them to the bot!
-//Feel free to ask for help! I (Arch1010#4338) would love to help! Or ping me in hackermon's discord! https://discord.gg/M5MSE9CvNM
+const commands = [ping, command2]; // Add your commands with commas to add them to the bot!
+// Join the Discord for support: https://discord.gg/M5MSE9CvNM
 
 const client = new Client({ intents: [] });
 const rl = createInterface({ input: process.stdin, output: process.stdout });
 
 
-client.on('interactionCreate', (interaction) => { //This is when the / command is used!
-  //This 'if'/'else if'/'else' statement allows you to add commands!
-  if (interaction.commandName === 'ping') { //commandName is auto generator by the interaction! Please change the text inside 
-    //to a command that you are using. 
-  interaction.reply('yes');  
-  } else if(interaction.commandName === 'command2') { //This is the example command's name!
-    interaction.reply("Example Command!");
-  } else { //A response if you forget to add the command here!
-    interaction.reply("This command's respone hasnt been added yet!");
+client.on('interactionCreate', (interaction) => {
+  if (interaction.commandName === 'ping') {
+    interaction.reply('yes');  
+  } else if(interaction.commandName === 'command2') { // This is the example command's name!
+    interaction.reply('example command');
+  } else { //A  response if you forget to add the command here!
+    interaction.reply('this command\'s response has not been added yet!');
   }
 });
 
@@ -55,5 +55,4 @@ const question = (q) => new Promise((resolve) => rl.question(q, resolve));
   await client.rest.put(Routes.applicationCommands(client.user.id), { body: commands }); //commands added to the variable will auto update!
 
   console.log('DONE | Application/Bot is up and running. DO NOT CLOSE THIS TAB UNLESS YOU ARE FINISHED USING THE BOT, IT WILL PUT THE BOT OFFLINE.');
-  //Do NOT disregard this message. Doing the opposite WILL turn the bot off and unresponsive to /commands!
 })();
