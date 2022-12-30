01803d4cbaa7bead2861d83b3b3704b18a2ebccd
hackermon <therealhackermon@gmail.com>
2022-12-30 09:42:29 -0500

    custom options example

--- app.js
+++ app.js
@@ -11,8 +11,36 @@ const ping = {
 
 // Command Example
 const command2 = {
-  name:'command2',
-  description:'yes'
+  name: 'command2',
+  description: 'yes',
+  options: [
+        {
+            "name": "animal",
+            "description": "The type of animal",
+            "type": 3,
+            "required": true,
+            "choices": [
+                {
+                    "name": "Dog",
+                    "value": "animal_dog"
+                },
+                {
+                    "name": "Cat",
+                    "value": "animal_cat"
+                },
+                {
+                    "name": "Penguin",
+                    "value": "animal_penguin"
+                }
+            ]
+        },
+        {
+            "name": "only_smol",
+            "description": "Whether to show only baby animals",
+            "type": 5,
+            "required": false
+        }
+    ]
 }
 
 const commands = [ping, command2]; // Add your commands with commas to add them to the bot!
